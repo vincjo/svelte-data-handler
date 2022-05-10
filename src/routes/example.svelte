258556d78f3df7e7666d887a09a8598599e7b1cb
vincjo <v.joseph3@gmail.com>
2022-05-10 11:32:47 +0200

    fix: page size

--- src/routes/example.svelte
+++ src/routes/example.svelte
@@ -2,10 +2,16 @@
 	import Index from '$components/Example/Index.svelte'
 </script>
 
-<a href="https://github.com/vincjo/svelte-data-handler/tree/master/src/components/Example" target="_blank">code &#10145;</a>
-<Index/>
+<section>
+
+	<a href="https://github.com/vincjo/svelte-data-handler/tree/master/src/components/Example" target="_blank">code &#10145;</a>
+
+	<Index/>
+
+</section>
 
 <style>
+	section{max-width:1040px;}
 	a{color:var(--tertiary);float:right;margin-right:80px;margin-top:-16px;font-size:18px;transition:all,0.2s;}
 	a:hover{color:var(--secondary)}
 </style>
